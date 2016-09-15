fee26f52c7d4d48f86dcf2c6c365907bd36824f5
johnestes4 <johnestes4@gmail.com>
2016-09-15 11:59:31 -0400

    switched to heroku hosted api

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -3,22 +3,22 @@ import axios from 'axios'
 function DogModel(){}
 
 DogModel.all = function(){
-  var request = axios.get("http://localhost:4000/dogs")
+  var request = axios.get("https://ratemydog-api.herokuapp.com/dogs")
   return request
 }
 
 DogModel.create = function(dog){
-  var request = axios.post("http://localhost:4000/dogs", dog)
+  var request = axios.post("https://ratemydog-api.herokuapp.com/dogs", dog)
   return request
 }
 
 DogModel.update = function(dogId, dogRating, dogBonus){
-  var request = axios.put(`http://localhost:4000/dogs/${dogId}`, {rating: dogRating, old_rating: dogRating, bonus: dogBonus})
+  var request = axios.put(`https://ratemydog-api.herokuapp.com/dogs/${dogId}`, {rating: dogRating, old_rating: dogRating, bonus: dogBonus})
   return request
 }
 
 DogModel.deleteDog = function(dogId){
-  var request = axios.delete(`http://localhost:4000/dogs/${dogId}`)
+  var request = axios.delete(`https://ratemydog-api.herokuapp.com/dogs/${dogId}`)
   return request
 }
 
--- src/models/Rate.js
+++ src/models/Rate.js
@@ -3,22 +3,22 @@ import axios from 'axios'
 function RateModel(){}
 
 RateModel.all = function(){
-  var request = axios.get("http://localhost:4000/rates")
+  var request = axios.get("https://ratemydog-api.herokuapp.com/rates")
   return request
 }
 
 RateModel.create = function(rate){
-  var request = axios.post("http://localhost:4000/rates", rate)
+  var request = axios.post("https://ratemydog-api.herokuapp.com/rates", rate)
   return request
 }
 
 RateModel.update = function(newId, newRating, newPettable, newHang, newNoDog){
-  var request = axios.put(`http://localhost:4000/rates/${newId}`, {rating: newRating, vote_pettable: newPettable, vote_hang: newHang, vote_nodog: newNoDog})
+  var request = axios.put(`https://ratemydog-api.herokuapp.com/rates/${newId}`, {rating: newRating, vote_pettable: newPettable, vote_hang: newHang, vote_nodog: newNoDog})
   return request
 }
 
 RateModel.deleteDog = function(rateId){
-  var request = axios.delete(`http://localhost:4000/rates/${rateId}`)
+  var request = axios.delete(`https://ratemydog-api.herokuapp.com/rates/${rateId}`)
   return request
 }
 
